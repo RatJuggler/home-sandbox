2f2954b025569f5970ec0cfda12f9f340e9367a0
John Chase <ratteal@gmail.com>
2013-04-25 09:14:12 +0100

    Tweak fork banner layer.

--- theme-changer.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<?php
-  // This page simply sets the theme cookie then redirects back to the referring page,
-  // which will then pick up the new cookie and load the appropriate theme CSS.
-  $theme = $_GET['theme'];
-  setcookie("theme", $theme, time() + 604800); // Cookie should last for a week.
-  header("Location: ".$_SERVER['HTTP_REFERER']);
-?>
